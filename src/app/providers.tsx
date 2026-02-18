2fd52d1449f00b348a8030bbc3a35aacd467f84b
JoSeHD3 <sebastian.bozek3@gmail.com>
2026-02-18 16:04:42 +0100

    Shadcn/ui sonner import

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,13 +2,17 @@
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactNode, useState } from 'react';
+import { Toaster } from 'sonner';
 
 export const Providers = ({ children }: { children: ReactNode }) => {
     const [queryClient] = useState(() => new QueryClient());
 
     return (
-        <QueryClientProvider client={queryClient}>
-            {children}
-        </QueryClientProvider>
+        <>
+            <QueryClientProvider client={queryClient}>
+                {children}
+            </QueryClientProvider>
+            <Toaster position="bottom-right" />
+        </>
     );
 };
