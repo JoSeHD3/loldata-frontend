05c9785d1c36a1fee3332e751155f69ec619cdd5
JoSeHD3 <sebastian.bozek3@gmail.com>
2026-03-12 17:01:11 +0100

    Added AuthGuard component for authorization handling

---
+++ src/lib/AuthGuard.tsx
@@ -0,0 +1,41 @@
+import { useUser } from '@/hooks/useUser';
+import { usePathname, useRouter } from 'next/navigation';
+import { ReactNode, useEffect } from 'react';
+
+interface AuthGuardProps {
+    children: ReactNode;
+    fallback?: ReactNode;
+    loginPath?: string;
+}
+
+const AuthGuard = ({
+    children,
+    fallback = <div className="">Loading...</div>,
+    loginPath = '/login',
+}: AuthGuardProps) => {
+    const { user, isLoading, isError } = useUser();
+    const router = useRouter();
+    const pathname = usePathname();
+
+    const isAuthPage =
+        pathname.startsWith(loginPath) || pathname.startsWith('/register');
+
+    useEffect(() => {
+        if (isAuthPage) return;
+
+        if (isError || (!isLoading && !user)) {
+            const searchParams = new URLSearchParams({ redirect: pathname });
+            router.replace(`${loginPath}?${searchParams.toString()}`);
+        }
+    }, [isError, isLoading, user, pathname, router, isAuthPage, loginPath]);
+
+    if (isLoading) {
+        return <>{fallback}</>;
+    }
+
+    if (!user) router.push(loginPath);
+
+    return <>{children}</>;
+};
+
+export { AuthGuard };
