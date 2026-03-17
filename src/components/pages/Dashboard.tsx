c738652f8b7b4f3a08e544f7c815e081073333cb
JoSeHD3 <sebastian.bozek3@gmail.com>
2026-03-17 23:23:09 +0100

    Fixed dashboard mock layout: aligned tabs to the end, fixed height of charts to match 3-stacked cards' height, fixed flex wrap with cards and last chart on layout

--- src/components/pages/Dashboard.tsx
+++ src/components/pages/Dashboard.tsx
@@ -8,7 +8,7 @@ const Dashboard = () => {
 
             <main className="flex-1 overflow-y-auto p-10">
                 <div className="mx-auto max-w-7xl space-y-8">
-                    <div className="flex flex-wrap gap-4">
+                    <div className="flex flex-wrap place-content-end gap-4">
                         {[
                             'Aggression',
                             'Farming',
@@ -20,7 +20,7 @@ const Dashboard = () => {
                         ].map(tab => (
                             <button
                                 key={tab}
-                                className="bg-sub1 text-muted hover:bg-sub1/20 text-sub4 rounded-full px-6 py-2 text-sm"
+                                className="bg-sub1 hover:bg-sub2/40 text-sub4 rounded-full px-6 py-2 text-sm"
                             >
                                 {tab}
                             </button>
@@ -28,32 +28,35 @@ const Dashboard = () => {
                     </div>
 
                     <div className="flex flex-wrap gap-8">
-                        <div className="bg-sub1 h-[300px] min-w-md flex-1 rounded-2xl" />
+                        <div className="bg-sub1 h-82 min-w-md flex-1 rounded-2xl" />
 
-                        <div className="bg-sub1 h-[300px] min-w-md flex-1 rounded-2xl" />
+                        <div className="bg-sub1 h-82 min-w-md flex-1 rounded-2xl" />
                     </div>
-
                     <div className="flex flex-wrap gap-8">
-                        <Card
-                            cardName={'AVERAGE GOLD PER MINUTE'}
-                            context={'357.25'}
-                            layoutColor={'#0e6ba8'}
-                            trend={'positive'}
-                        />
-                        <Card
-                            cardName={'AVERAGE CS PER MINUTE'}
-                            context={'8.2'}
-                            layoutColor={'#0e6ba8'}
-                            trend={'positive'}
-                        />
-                        <Card
-                            cardName={'AVERAGE DAMAGE DONE TO PLAYERS'}
-                            context={'25773'}
-                            layoutColor={'#0e6ba8'}
-                            trend={'negative'}
-                        />
+                        <div className="flex flex-1 flex-wrap gap-8">
+                            <Card
+                                cardName={'AVERAGE GOLD PER MINUTE'}
+                                context={'357.25'}
+                                layoutColor={'#0e6ba8'}
+                                trend={'positive'}
+                            />
+
+                            <Card
+                                cardName={'AVERAGE CS PER MINUTE'}
+                                context={'8.2'}
+                                layoutColor={'#0e6ba8'}
+                                trend={'positive'}
+                            />
+
+                            <Card
+                                cardName={'AVERAGE DAMAGE DONE TO PLAYERS'}
+                                context={'25773'}
+                                layoutColor={'#0e6ba8'}
+                                trend={'negative'}
+                            />
+                        </div>
+                        <div className="bg-sub1 h-82 min-w-md flex-2 rounded-2xl" />
                     </div>
-                    <div className="bg-sub1 col-span-2 h-[300px] rounded-2xl" />
                 </div>
             </main>
         </div>
