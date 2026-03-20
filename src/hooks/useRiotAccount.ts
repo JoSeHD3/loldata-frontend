adfccba8f6e384ec8000bae58bbae07a0e1993a4
JoSeHD3 <sebastian.bozek3@gmail.com>
2026-03-20 14:52:18 +0100

    Fix for useRiotAccount hook, now it returns array of accounts found in db.

--- src/hooks/useRiotAccount.ts
+++ src/hooks/useRiotAccount.ts
@@ -6,7 +6,7 @@ const useRiotAccount = () => {
         data: riotAccount,
         isLoading,
         isError,
-    } = useQuery<RiotAccount>({
+    } = useQuery<RiotAccount[]>({
         queryKey: ['riotAccount'],
         queryFn: getRiotAccount,
         staleTime: 1000 * 60 * 5,
@@ -15,7 +15,7 @@ const useRiotAccount = () => {
     return { riotAccount, isLoading, isError };
 };
 
-const getRiotAccount = async (): Promise<RiotAccount> => {
+const getRiotAccount = async (): Promise<RiotAccount[]> => {
     const res = await fetch('/api/riot-account', { credentials: 'include' });
 
     if (!res.ok) throw new Error('Failed to fetch riot account');
